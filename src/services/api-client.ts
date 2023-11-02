7bd0f576ba64ee07d9d6964db193859761769847
Błażej Mzyk <blazej.mzyk@gmail.com>
2023-11-02 13:45:08 +0100

    Fetch the games

---
+++ src/services/api-client.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: 'https://api.rawg.io/api',
+    params: {
+        key: '1de9b8d6dc0a43b1bba177c96ceb1b1c' //dzięki temu key będzie w query string kadego http request wysłanym do naszego backendu
+    }
+})
