4e6721c432b1d8fcaea87dea0abb068d0b197cc7
Błażej Mzyk <blazej.mzyk@gmail.com>
2023-11-03 11:50:47 +0100

    Displaying platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,46 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  //przyporządkowujemy ikony nazwom platform. Nie robimy ifami, ale uzyjemy obiektu jako mapy
+  //mapujemy slug'i, poniewaz nie powinny się zmieniać
+  const iconMap: { [key: string]: IconType } = { //index signuture (niezaleznie od nazw key) dla typescripta + typ keys z biblioteki ikon
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={1}> 
+        {/* wyświetlamy ikony horyzontalnie + verical margin 1x theme space*/}
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color='gray.500' />
+        // <Text>{platform.name}</Text> -> wyświetlanie nazw platform
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
