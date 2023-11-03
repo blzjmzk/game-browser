1377a741906baa06e15e710c46a0f23154084a5a
Błażej Mzyk <blazej.mzyk@gmail.com>
2023-11-03 13:08:21 +0100

    Get optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,12 @@
+// Narzedzie do przycinania obrazow. Nie chcemy wyświetlać duzych obrazów jako miniatury w kartach. Api które wykorzystujemy obsługuje przycinanie obrazów przez dodanie w url obrazów crop/
+//np:
+//https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
+//https://media.rawg.io/media/crop/600/400/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
+
+const getCroppedImageUrl = (url: string) => {
+  const target = "media/";
+  const index = url.indexOf(target) + target.length; //znajdujemy index początku media/. aby zwrócić index końca media (po którym chcemy dodać crop/) musimy dodać długość tego fragmentu
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index); //zwracamy wszystkie znaki od początku do media/ łącznie z media/. Potem fragment z croop a następnie resztę url od indexu
+};
+
+export default getCroppedImageUrl;
