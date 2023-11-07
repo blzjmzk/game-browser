ffc6791b576fb7f4151bd29fa30975f8e11d0074
Błażej Mzyk <blazej.mzyk@gmail.com>
2023-11-07 12:01:42 +0100

    Troubleshooting

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -14,6 +14,15 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = (selectedGenre: Genre | null) => useData<Game>('/games', { params: { genres: selectedGenre.id}, [selectedGenre?.id]});
+const useGames = (selectedGenre: Genre | null) =>
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: selectedGenre?.id,
+      },
+    },
+    [selectedGenre?.id]
+  );
 
 export default useGames;
