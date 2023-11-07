3da4daa34bf24fd22bdc9508ad953dbcfaf24b81
Błażej Mzyk <blazej.mzyk@gmail.com>
2023-11-07 12:45:38 +0100

    Build platform selector

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
+import PlatformSelector from "./components/PlatformSelector";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null); //inicjujemy z null, bo wyjściowo zaden gatunek nie jest wybrany. Musimy równie dodać generic type argument, w przeciwnym razie nie moglibyśmy ustalić gatunku na nic innego niz null
@@ -29,6 +30,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
+        <PlatformSelector />
         <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
