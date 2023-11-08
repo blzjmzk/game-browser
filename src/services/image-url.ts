09bbe8e2fb7aacd87c208dd99401ab745fd7e372
Błażej Mzyk <blazej.mzyk@gmail.com>
2023-11-08 11:16:17 +0100

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -3,7 +3,11 @@
 //https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
 //https://media.rawg.io/media/crop/600/400/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
 
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp';
+
 const getCroppedImageUrl = (url: string) => {
+  if (!url) return noImage;
+
   const target = "media/";
   const index = url.indexOf(target) + target.length; //znajdujemy index początku media/. aby zwrócić index końca media (po którym chcemy dodać crop/) musimy dodać długość tego fragmentu
   return url.slice(0, index) + "crop/600/400/" + url.slice(index); //zwracamy wszystkie znaki od początku do media/ łącznie z media/. Potem fragment z croop a następnie resztę url od indexu
