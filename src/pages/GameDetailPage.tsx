edfe289e666eb553bc6302ed1025e5e3a952d2de
Błażej Mzyk <blazej.mzyk@gmail.com>
2023-12-04 12:42:26 +0100

    Expandable text component added

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import { Spinner } from "@chakra-ui/spinner";
 import { Heading, Text } from "@chakra-ui/react";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -13,9 +14,7 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-<Text>
-{game.description_raw}
-</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
