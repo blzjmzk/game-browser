2519d4d938d4d5f787889cfd5457da97430cc41e
Błażej Mzyk <blazej.mzyk@gmail.com>
2023-12-04 14:07:34 +0100

    Adding default export

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,10 +1,10 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
-import { Game } from "../entities/Game";
-import PlatformIconList from "./PlatformIconList";
-import CriticScore from "./CriticScore";
-import getCroppedImageUrl from "../services/image-url";
-import Emoji from "./Emoji";
 import { Link } from "react-router-dom";
+import Game from "../entities/Game";
+import getCroppedImageUrl from "../services/image-url";
+import CriticScore from "./CriticScore";
+import Emoji from "./Emoji";
+import PlatformIconList from "./PlatformIconList";
 
 //zapewniamy game object jako prop do tego komponentu z effect hook
 interface Props {
